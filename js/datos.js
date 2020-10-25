00d73e5456be153ec6b8ec4f10d1921416881215
alvaro0503 <alvarovp0503@gmail.com>
2020-10-25 17:45:54 +0100

    mover funcion pintar a eventos

--- js/datos.js
+++ js/datos.js
@@ -1,5 +1,3 @@
-//añadir tarea------------------------------------------
-
 
 var listaTareas = new Array();
 
@@ -8,56 +6,6 @@ listaTareas = [{ 'idTarea': 0, 'titulo': 'Estudiar Javascript', 'prioridad': 'ur
 { 'idTarea': 3, 'titulo': 'Salir a comer', 'prioridad': 'mensual' }
 ];
 
-var contador = 4
-
-function pintarTareas(pListaTareas) {
-
-	document.getElementById('notareas').innerHTML = ""
-
-	var contenedor = document.getElementById('tareas')
-
-	contenedor.innerHTML = "";
-
-	for (var i = 0; i < pListaTareas.length; i++) {
-		var articulo = document.createElement('article')
-
-		articulo.id = pListaTareas[i].idTarea
-
-		articulo.className = pListaTareas[i].prioridad
-
-		var tituloTarea = document.createElement('h2')
-
-		var textoTarea = document.createTextNode(pListaTareas[i].titulo)
-
-		var btnEliminar = document.createElement('a')
-
-		btnEliminar.href = "#"
-
-		btnEliminar.title = "Eliminar"
-
-		btnEliminar.setAttribute("onclick", "borrarTarea(" + pListaTareas[i].idTarea + ")")
-
-		var textoEliminar = document.createTextNode('Eliminar')
-
-		//metemos todo en su cajita---------------------------
-
-		tituloTarea.appendChild(textoTarea)
-
-		articulo.appendChild(tituloTarea)
-
-		btnEliminar.appendChild(textoEliminar)
-
-		articulo.appendChild(btnEliminar)
-
-		contenedor.appendChild(articulo)
-
-
-	}
-}
-
-pintarTareas(listaTareas)
-
-//borrar tarea------------------------------------------------
 
 
 
