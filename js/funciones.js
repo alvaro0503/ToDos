4d3964c3ff4935fc976668cb621f909297640f34
alvaro0503 <alvarovp0503@gmail.com>
2020-10-25 18:20:00 +0100

    funcion borrar

--- js/funciones.js
+++ js/funciones.js
@@ -7,3 +7,14 @@ function addTarea(pTitulo, pPrioridad) {
 
 }
 
+function borrarTarea(pId) {
+    for (var i = 0; i < listaTareas.length; i++) {
+        if (listaTareas[i].idTarea == pId) {
+            var registroBorrar = i
+        }
+    }
+
+    listaTareas.splice(registroBorrar, 1)// metodo .splice eleimina elementos del array(incluido a si mismo en el numero)
+
+    pintarTareas(listaTareas)
+}
