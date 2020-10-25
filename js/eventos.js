9f8dc80ff00f16678ba358cdb4142bcfd02fe518
alvaro0503 <alvarovp0503@gmail.com>
2020-10-25 18:49:52 +0100

    recoger para filtrar

--- js/eventos.js
+++ js/eventos.js
@@ -5,7 +5,7 @@ var selectorPrioridad = document.getElementById('prioridadSelect')
 var busqueda = document.getElementById('search')
 
 btnGuardar.addEventListener('click', recogerTarea)
-
+busqueda.addEventListener('keyup', recogerBusqueda)
 
 
 
@@ -31,5 +31,47 @@ function recogerTarea(event) {
 
         document.getElementById('mensaje').innerText = "Debes completar todos los campos correctamente"
     }
+}
+
+function recogerPrioridad(event) {
+
+    var prioridad = event.target.value
+
+    if (prioridad == "") {
+
+        pintarTareas(listaTareas)
+
+    } else {
+
+        var listaFiltrada = filtrarTareas(prioridad)
+
+        pintarTareas(listaFiltrada)
+    }
+
+
+}
+
+function recogerBusqueda(event) {
+
+    var palabraBuscar = event.target.value
+
+    var listaBusqueda = busqueda(palabraBuscar)
+
+    if (listaBusqueda.length != 0) {
+
+
+
+        pintarTareas(listaBusqueda)
+
+    } else {
+
+        document.getElementById('tareas').innerHTML = ""
+
+        document.getElementById('notareas').innerHTML = "<article>No hay tareas con esos parametros</article>"
+    }
+
+
+
+    console.log(palabraBuscar)
 }
 
